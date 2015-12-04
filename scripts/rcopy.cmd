c66a72a06f00a21502e68a5d54a99226cc282264
unknown <fanyunfeng@hc360.com>
2015-12-04 21:14:37 +0800

    added script to copy files.

---
+++ scripts/rcopy.cmd
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set LOCATION=%~dp0
+set HOSTFILE=%LOCATION%\hosts.list
+
+
+for /f %%i in ( %HOSTFILE% ) do (
+	if "%%i"=="" continue
+
+	set host=%%i
+
+	if "!host:~0,1!"=="#" (
+		echo "common out %%i"
+	) else (
+		echo "xcopy to %%i:"
+		mkdir \\%%i\c$%~pn1
+		xcopy %1 \\%%i\c$%~pn1 /E /F
+	)
+)
