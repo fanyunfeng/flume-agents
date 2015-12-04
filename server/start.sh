b5a54d7fc1af8923eb9515de24df3b439e07818e
root <root@monitor_server_2.(none)>
2015-12-04 17:05:23 +0800

    added scripts for server.

--- server/start.sh
+++ server/start.sh
@@ -2,8 +2,12 @@ bin=`which $0`
 bin=`dirname ${bin}`
 bin=`cd "$bin"; pwd`
 
+cd ${bin}
+
 #load config
 . ${bin}/../etc/config.sh
 
+
 #
 su  ${HADOOP_USER} -c "FLUME_JAVA_OPTS="-Xmx2g" ${FLUME_HOME}/bin/flume-ng agent --conf ${FLUME_HOME}/conf --conf-file ${bin}/agents.conf --name a1 -Dflume.root.logger=DEBUG,LOGFILE"
+cd -
