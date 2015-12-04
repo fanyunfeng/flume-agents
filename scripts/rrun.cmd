4af25448f03ef880ce0f7665092b29f44e5018bc
unknown <fanyunfeng@hc360.com>
2015-12-04 20:43:56 +0800

    added script to set environment variable.

---
+++ scripts/rrun.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+set LOCATION=%~dp0
+set HOSTFILE=%LOCATION%\hosts.list
+
+
+for /f %%i in ( %HOSTFILE% ) do (
+	if "%%i"=="" continue
+
+	set host=%%i
+
+	if "!host:~0,1!"=="#" (
+		echo "common out %%i"
+	)	else (
+		echo "install on %%i:"
+		psexec \\%%i -c %1
+	)
+)
