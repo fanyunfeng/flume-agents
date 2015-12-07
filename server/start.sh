f0e795f9aafb17ae6981a0099b1c59f3afa04924
liuqingshun <liuqingshun@hc360.com>
2015-12-07 15:24:17 +0800

    change file size.

--- server/start.sh
+++ server/start.sh
@@ -26,4 +26,4 @@ fi
 
 
 #
-su  ${HADOOP_USER} -c "FLUME_JAVA_OPTS="-Xmx2g" ${FLUME_HOME}/bin/flume-ng agent --conf ${FLUME_HOME}/conf --conf-file ${bin}/agents.conf --name a1 -Dflume.root.logger=DEBUG,LOGFILE"
+exec su  ${HADOOP_USER} -c "FLUME_JAVA_OPTS="-Xmx2g" ${FLUME_HOME}/bin/flume-ng agent --conf ${FLUME_HOME}/conf --conf-file ${bin}/agents.conf --name a1 -Dflume.root.logger=DEBUG,LOGFILE"
