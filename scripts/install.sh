11be99be5978a8b6e3eeac760343e0ea8f203152
admin <admin@hc360.com>
2016-05-18 10:28:52 +0800

    revert.

--- scripts/install.sh
+++ scripts/install.sh
@@ -29,6 +29,6 @@ echo create service
 ${bin}/csrv2.sh $1
 
 
-#echo #################################
-#echo create schedule 
-#${bin}/rsch.sh $1 /c/opt/flume-agents/client/wrmfile.bat
+echo #################################
+echo create schedule 
+${bin}/rsch.sh $1 /c/opt/flume-agents/client/wrmfile.bat
