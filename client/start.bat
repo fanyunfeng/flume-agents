ff5429437d12cfa2750d5c9fcade03e5a75e719a
admin <admin@hc360.com>
2016-09-28 16:20:26 +0800

    use HcseGangliaServer.

--- client/start.bat
+++ client/start.bat
@@ -4,8 +4,13 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 set LOCATION=%~dp0
 
+set IP=%~n1
+REM echo %IP%
+
+
 set JAVABIN="%JAVA_HOME%\bin\java"
-set OPTION=-Dflume.root.logger=DEBUG,LOGFILE -Dflume.monitoring.type=ganglia -Dflume.monitoring.hosts=192.168.60.124:8649,192.168.60.62:8649 
+set OPTION=-Dflume.root.logger=DEBUG,LOGFILE 
+set MONITOR=-Dflume.monitoring.type=hcse.flume.HcseGangliaServer -Dflume.monitoring.hosts=192.168.60.124:8649,192.168.60.62:8649 -Dflume.monitoring.hostname=%IP%
 set CLASSOPTION=%FLUME_HOME%\conf
 set RUNCLASS=org.apache.flume.node.Application
 
@@ -16,5 +21,6 @@ for %%i in (%FLUME_HOME%\lib\*.jar) do (
 
 REM echo %CLASSOPTION%
 
-%JAVABIN% %OPTION% -cp %CLASSOPTION% %RUNCLASS% --conf-file %LOCATION%%1 --name a1
+REM 
+%JAVABIN% %OPTION% %MONITOR% -cp %CLASSOPTION% %RUNCLASS% --conf-file %LOCATION%%1 --name a1
 
