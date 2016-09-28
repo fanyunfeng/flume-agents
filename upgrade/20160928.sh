357cd24ab22c786c72798153053dd184462c68e3
admin <admin@hc360.com>
2016-09-28 16:51:20 +0800

    sleep 3 second after stop service.

--- upgrade/20160928.sh
+++ upgrade/20160928.sh
@@ -2,6 +2,8 @@
 
 sc stop hc.flume
 
+sleep 3
+
 scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/flume-ng-core-1.6.0.jar /c/opt/apache-flume-1.6.0-bin/lib
 
 scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/hcse-flume-tools-1.0-SNAPSHOT.jar /c/opt/apache-flume-1.6.0-bin/lib
