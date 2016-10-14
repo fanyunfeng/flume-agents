8343b093f1886cb4e84662f21ee1e12ee1bf5065
root <root@hcse-web-55.(none)>
2016-10-14 18:47:09 +0800

    added upgrade script for Linux.

--- upgrade/20160928.sh
+++ upgrade/20160928.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-sc stop hc.flume
+scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/flume-ng-core-1.6.0.jar /opt/apache-flume-1.6.0-bin/lib
+scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/hcse-flume-tools-1.0-SNAPSHOT.jar /opt/apache-flume-1.6.0-bin/lib
 
-sleep 3
-
-scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/flume-ng-core-1.6.0.jar /c/opt/apache-flume-1.6.0-bin/lib
-
-scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/hcse-flume-tools-1.0-SNAPSHOT.jar /c/opt/apache-flume-1.6.0-bin/lib
-
-
-sc start hc.flume
+/app/daemontools/bin/svc -k /app/service/flume.client
---
+++ upgrade/20160928.win.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sc stop hc.flume
+
+sleep 3
+
+scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/flume-ng-core-1.6.0.jar /c/opt/apache-flume-1.6.0-bin/lib
+
+scp admin@192.168.119.94:/share/install/opt/apache-flume-1.6.0-bin/lib/hcse-flume-tools-1.0-SNAPSHOT.jar /c/opt/apache-flume-1.6.0-bin/lib
+
+
+sc start hc.flume
